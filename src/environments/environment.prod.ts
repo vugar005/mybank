11aa577703c2bcc0958cd568a2d3cfbc13a76c40
Vüqar Abdullayev <vugar.abdullayev@neuron.az>
2019-04-04 10:58:52 +0400

    firebase hosting added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA0DSJr8UjCGO4BAW40LbeN-Mv8uO6h5BE",
+    authDomain: "personal-website-7165a.firebaseapp.com",
+    databaseURL: "https://personal-website-7165a.firebaseio.com",
+    projectId: "personal-website-7165a",
+    storageBucket: "personal-website-7165a.appspot.com",
+    messagingSenderId: "259019036947"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA0DSJr8UjCGO4BAW40LbeN-Mv8uO6h5BE",
+    authDomain: "personal-website-7165a.firebaseapp.com",
+    databaseURL: "https://personal-website-7165a.firebaseio.com",
+    projectId: "personal-website-7165a",
+    storageBucket: "personal-website-7165a.appspot.com",
+    messagingSenderId: "259019036947"
+  }
 };
 
 /*
